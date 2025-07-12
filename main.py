81374edb5affce75b5355a877524b8eb4c6c76f4
TheDoSITA <emanuele.marconi1@gmail.com>
2025-07-12 16:19:32 +0200

    base game completed

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()  # Initialize the pygame library
@@ -10,19 +14,45 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")  
     dt = 0 # Initialize delta time
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player=Player(x, y) 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()  # Create a group for asteroids
+    shots = pygame.sprite.Group() 
+    Asteroid.containers = (asteroids, updatable, drawable)   
+    AsteroidField.containers = (updatable)  # Set containers for the asteroid field
+    asteroid_field = AsteroidField()
+    Shot.containers = (shots, updatable, drawable)  # Set containers for shots
+    Player.containers = (updatable, drawable)
+    player=Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)  # Create a player object at the center of the screen
+    
+    
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
            
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if player.collision(asteroid):
+                sys.exit("GAME OVER!")
+            
+            for shot in shots:
+                if asteroid.collision(shot):
+                    shot.kill()  # Remove the shot and the asteroid
+                    asteroid.split()
+            
+
         screen.fill("black") # Fill the screen with black
-        player.draw(screen)
+                
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip() #Refresh the screen, should run last
-        dt = clock.tick(60)/1000  # Control the frame rate to 60 FPS
-             
+        
+        dt = clock.tick(60)/1000  # limit  the frame rate to 60 FPS
+            
          
 
 
--- player.py
+++ player.py
@@ -1,10 +1,15 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
+
 class Player(CircleShape):
+      
+    
     def __init__(self, x, y):        # Call the parent class constructor
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     # in the player class
     def triangle(self):
@@ -17,4 +22,36 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), width=2)
+    
+   
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        self.timer -= dt
+        
+        if keys[pygame.K_a]:
+            self.rotate(-dt)  
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+       
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+
+    def move(self, dt):
+        forward = pygame.Vector2(0,1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED* dt
+    
+    def shoot(self):
+        if self.timer > 0:
+            return
+        self.timer = PLAYER_SHOOT_COOLDOWN
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        
         
